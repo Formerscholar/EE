aa7ed6c87b23cb3977ebb61545309cf9ecda958e
Formerscholar <59386936+Formerscholar@users.noreply.github.com>
2022-12-22 16:11:30 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace DemoForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process[] processes = Process.GetProcesses();
+
+            Form2 form2 = new Form2();
+
+            form2.Param = processes;
+
+            form2.Show();
+
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoForm
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public Process[] Param { get; internal set; }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+
+            // 遍历进程列表
+            foreach (Process process in Param)
+            {
+                // 创建一个 ListViewItem 对象
+                ListViewItem item = new ListViewItem(process.ProcessName);
+                //item.SubItems.Add(process.ProcessName);
+                // 设置进程 ID
+                item.SubItems.Add(process.Id.ToString());
+
+                // 将 ListViewItem 添加到 ListView 控件中
+                listView1.Items.Add(item);
+            }
+        }
+    }
+}
